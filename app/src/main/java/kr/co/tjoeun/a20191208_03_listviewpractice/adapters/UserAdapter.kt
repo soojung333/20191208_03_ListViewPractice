2a268b5bd68b84bfce9bbbdb99c761a237f2b607
blessme333 <blessme333@gmail.com>
2019-12-08 14:56:32 +0900

    UserAdapter 기초 작업  - 기본 생성자 활용  - 상속 개념 활용  - null 체크 후 절대 null이 아니라는 !! 활용

--- app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/adapters/UserAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/adapters/UserAdapter.kt
@@ -8,24 +8,22 @@ import android.widget.ArrayAdapter
 import kr.co.tjoeun.a20191208_03_listviewpractice.R
 import kr.co.tjoeun.a20191208_03_listviewpractice.datas.User
 
-class UserAdapter(context: Context, resId: Int, list: ArrayList<User>) : ArrayAdapter<User>(context, resId, list) {
+class UserAdapter(context:Context, resId:Int, list: ArrayList<User>) : ArrayAdapter<User>(context, resId, list) {
 
     val mContext = context
     val mList = list
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val tempRow = convertView
+        var tempRow = convertView
         if (tempRow == null) {
-            tempRow =inflate(R.layout.user_list_item, root: null)
+            tempRow = inf.inflate(R.layout.user_list_item, null)
         }
-
         val row = tempRow!!
 
 //        상황에 맞는 데이터를 user_list_item에 뿌려주기
 
-
+        return row
     }
 
 }
