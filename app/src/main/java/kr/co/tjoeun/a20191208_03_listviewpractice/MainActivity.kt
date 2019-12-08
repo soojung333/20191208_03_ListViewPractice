f72b3e5f8a7221ab31da0a1cd77cfbc014992877
blessme333 <blessme333@gmail.com>
2019-12-08 13:58:35 +0900

    데이터클래스 기본생성자 추가

--- app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
@@ -2,11 +2,17 @@ package kr.co.tjoeun.a20191208_03_listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kr.co.tjoeun.a20191208_03_listviewpractice.datas.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tempUser = User(inputName = "이수정", inputAddress = "경기도 고양시", isWomanOk = true)
+
+        Log.d("사용자주소", tempUser.address)
     }
 }
---
+++ app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/datas/User.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.a20191208_03_listviewpractice.datas
+
+class User(inputName:String, inputAddress:String, isWomanOk:Boolean) {
+
+
+
+    var name =  inputName
+    var address = inputAddress
+    var isWoman = isWomanOk
+
+
+}
